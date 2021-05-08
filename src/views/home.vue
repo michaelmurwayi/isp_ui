17379a7f49f2d9ddb2328a944ab2db4cf89f94ea
michaelmurwayi <mikemurwayi7@gmail.com>
2021-05-08 12:42:35 +0300

    adding intro div in home page

--- src/views/home.vue
+++ src/views/home.vue
@@ -24,7 +24,7 @@
 
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-app-bar-title>Title</v-app-bar-title>
+      <v-app-bar-title class="mask pattern-8">SwiftNet</v-app-bar-title>
 
       <v-spacer></v-spacer>
 
@@ -45,60 +45,67 @@
         <v-flex class="subnav-content">
         <div class="nav-content">
           <div ><router-link class="navlinks" to="/" style="color:white">Home</router-link></div>
-          <div ><router-link class="navlinks" to="products" style="color:white"> Products</router-link></div>
-          <div ><router-link class="navlinks" to="services" style="color:white"> Services</router-link></div>
-          <div ><router-link class="navlinks" to="about" style="color:white"> About Us</router-link></div>
+          <div ><router-link class="navlinks" to="products" style="color:white"> Login</router-link></div>
+          <div ><router-link class="navlinks" to="services" style="color:white"> Register</router-link></div>
+          <div ><router-link class="navlinks" to="about" style="color:white"> Account</router-link></div>
         </div>
       </v-flex>
     </v-div>
-
       </template>
     </v-app-bar>
+
   </v-card>
     <!-- end of page navigation div -->
 
-    <!-- start of header div -->
-    <!-- <div class="mask pattern-8 header col-md-12">
-      <video autoplay muted loop class="mask pattern-8">
-      <source src="../assets/header.mp4"  type="video/mp4">
-    </video>
-    </div>
-     -->
    
     <!-- end of header div -->
 
     <!-- begin of page intro -->
-    <div class="intro black">
-      <v-col
-          cols="12"
-          sm="12"
+  <div>
+  <div class="col-md-12 text-center">
+    <v-carousel
+    class="intro"
+    cycle
+    height="200"
+    hide-delimiters
+    :show-arrows="false"
+  >
+    <template v-slot:prev="{ on, attrs }">
+      <v-btn
+        color="success"
+        v-bind="attrs"
+        v-on="on"
+      >Previous slide</v-btn>
+    </template>
+    <template v-slot:next="{ on, attrs }">
+      <v-btn
+        color="info"
+        v-bind="attrs"
+        v-on="on"
+      >Next slide</v-btn>
+    </template>
+    <v-carousel-item
+      v-for="(slide, i) in slides"
+      :key="i"
+    >
+      <v-sheet
+        :color="colors[i]"
+        height="100%"
+      >
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
         >
-         <md-card>
-      <md-card-media-cover md-solid>
-        <md-card-media md-ratio="1:1">
-          <img src="/assets/examples/card-sky.jpg" alt="Skyscraper">
-        </md-card-media>
-
-        <md-card-area>
-          <md-card-header>
-            <span class="md-title">Solid background</span>
-            <span class="md-subhead">1/1 image</span>
-          </md-card-header>
-
-          <md-card-actions>
-            <md-button class="md-icon-button">
-              <md-icon>favorite</md-icon>
-            </md-button>
-
-            <md-button class="md-icon-button">
-              <md-icon>share</md-icon>
-            </md-button>
-          </md-card-actions>
-        </md-card-area>
-      </md-card-media-cover>
-    </md-card>
-        </v-col>
-    </div>
+          <div class="display-3">
+            {{ slide }} Slide
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+  </div>
+  </div>
     <!-- end of page intro -->
   </v-container>
     </v-container>
@@ -107,14 +114,31 @@
 
 <script>
 export default {
-  
+   data () {
+      return {
+        colors: [
+          'indigo',
+          'warning',
+          'pink darken-2',
+          'red lighten-1',
+          'deep-purple accent-4',
+        ],
+        slides: [
+          'First',
+          'Second',
+          'Third',
+          
+        ],
+      }
+    },
   }
 </script>
 
 <style>
 .main{
   background-image: url("../assets/background.png");
-  position: fixed;
+  position: absolute;
+  bottom: 0px;
   height: 100%;
   width: 80vw;
   border-radius: 0px;
@@ -160,6 +184,9 @@ video{
   text-align: center;
   border-bottom: 3px solid transparent;
   display: flex;
+  font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+  font-size: 2vh;
+  font-weight: 200px;
   left: 0;
   padding: auto;
 }
@@ -185,7 +212,12 @@ a:hover,
   transition: width 2.4s linear;
   transition-property: border-bottom;
 }
-
+.intro{
+  position: absolute;
+  background: transparent;
+  top:165px;
+  left: 0px;
+}
 @media only screen and (max-width: 768px) {
   video{
   position: absolute;
